25c0b148ce4ae2e936df91d5578e49cc0a912859
vinicius <viniciusarashiro0@gmail.com>
2024-02-20 20:31:54 -0300

    feat: list, filters and pagination

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,63 @@
-import { FileDown, MoreHorizontal, Plus, Search } from 'lucide-react'
+import { FileDown, Filter, MoreHorizontal, Plus, Search } from 'lucide-react'
 import { Header } from './components/header'
 import { Tabs } from './components/tabs'
 import { Button } from './components/ui/button'
 import { Control, Input } from './components/ui/input'
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from './components/ui/table'
 import { Pagination } from './components/pagination'
+import { useQuery, keepPreviousData } from '@tanstack/react-query'
+import { useSearchParams } from 'react-router-dom'
+import { useState } from 'react'
+
+export interface TagResponse {
+  first: number
+  prev: number | null
+  next: number
+  last: number
+  pages: number
+  items: number
+  data: Tag[]
+}
+
+export interface Tag {
+  title: string
+  amountOfVideos: number
+  id: string
+}
+
 
 export function App() {
+  const [searchParams, setSearchParams] = useSearchParams()
+  const urlFilter = searchParams.get('filter') ?? ''
+
+  const [filter, setFilter] = useState(urlFilter)
+
+  const page = searchParams.get('page') ? Number(searchParams.get("page")) : 1
+
+  const { data: tagsResponse, isLoading } = useQuery<TagResponse>({
+    queryKey: ['get-tags', urlFilter, page],
+    queryFn: async () => {
+      const response = await fetch(`http://localhost:3333/tags?_page=${page}&_per_page=10&title=${urlFilter}`)
+      const data = await response.json()
+
+      return data
+    },
+    placeholderData: keepPreviousData,
+    staleTime: 1000 * 60
+  })
+
+  function handleFilter() {
+    setSearchParams(params => {
+      params.set('page', '1')
+      params.set('filter', filter)
+
+      return params
+    })
+  }
+
+  if (isLoading) {
+    return null
+  }
 
   return (
     <div className="py-10 space-y-8">
@@ -24,10 +75,17 @@ export function App() {
         </div>
 
         <div className='flex items-center justify-between'>
-          <Input variant='filter'>
-            <Search className='size-3' />
-            <Control placeholder='Search tags...' />
-          </Input>
+          <div className='flex flex-items'>
+            <Input variant='filter'>
+              <Search className='size-3' />
+              <Control placeholder='Search tags...' onChange={e => setFilter(e.target.value)} value={filter} />
+            </Input>
+
+            <Button onClick={handleFilter}>
+              <Filter className='size-3' />
+              Filter
+            </Button>
+          </div>
 
           <Button>
             <FileDown className='size-3' />
@@ -45,18 +103,18 @@ export function App() {
             </TableRow>
           </TableHeader>
           <TableBody>
-            {Array.from({ length: 10 }).map((value, index) => {
+            {tagsResponse?.data.map((tag) => {
               return (
-                <TableRow key={index}>
+                <TableRow key={tag.id}>
                   <TableCell></TableCell>
                   <TableCell>
                     <div className='flex flex-col gap-0.5'>
-                      <span className='font-medium'>React</span>
-                      <span className='text-xs text-zinc-500'>a22043d4-c23d-4ad2-a47e-a40c89c6380c</span>
+                      <span className='font-medium'>{tag.title}</span>
+                      <span className='text-xs text-zinc-500'>{tag.id}</span>
                     </div>
                   </TableCell>
                   <TableCell className='text-zinc-300'>
-                    13 vídeo(s)
+                    {tag.amountOfVideos} vídeo(s)
                   </TableCell>
                   <TableCell className='text-right'>
                     <Button size='icon'>
@@ -69,7 +127,8 @@ export function App() {
           </TableBody>
         </Table>
 
-        <Pagination />
+        {tagsResponse && <Pagination pages={tagsResponse.pages} items={tagsResponse.items} page={page} />}
+
       </main>
 
     </div>
